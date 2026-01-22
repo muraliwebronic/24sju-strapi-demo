5aa7dc6eeee244acb8e5236d09c62307de03712e
muraliwebronic <muralikrishna.webronic@gmail.com>
2026-01-22 12:51:25 +0530

    cross

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,18 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  
+  // REPLACE 'strapi::cors' WITH THIS BLOCK:
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // This allows ALL domains (including localhost)
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
